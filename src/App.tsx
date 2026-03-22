92786ec9a67f0fd234c886725637b00d63ac22d6
yiyang01 <yiyang01@gaotu.cn>
2026-03-22 21:20:56 +0800

    fix: 将部署路径从根路径改为 /home

--- src/App.tsx
+++ src/App.tsx
@@ -6,42 +6,47 @@ const NavigationPage = lazy(() => import('./pages/NavigationPage'));
 const ManagementPage = lazy(() => import('./pages/ManagementPage'));
 const Demo1Page = lazy(() => import('./pages/lab/Demo1Page'));
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: (
+            <Suspense fallback={<div>Loading...</div>}>
+              <NavigationPage />
+            </Suspense>
+          ),
+        },
+        {
+          path: 'manage',
+          element: (
+            <Suspense fallback={<div>Loading...</div>}>
+              <ManagementPage />
+            </Suspense>
+          ),
+        },
+        {
+          path: 'lab/demo1',
+          element: (
+            <Suspense fallback={<div>Loading...</div>}>
+              <Demo1Page />
+            </Suspense>
+          ),
+        },
+      ],
+    },
+    {
+      path: '*',
+      element: <Navigate to="/" replace />,
+    },
+  ],
   {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: (
-          <Suspense fallback={<div>Loading...</div>}>
-            <NavigationPage />
-          </Suspense>
-        ),
-      },
-      {
-        path: 'manage',
-        element: (
-          <Suspense fallback={<div>Loading...</div>}>
-            <ManagementPage />
-          </Suspense>
-        ),
-      },
-      {
-        path: 'lab/demo1',
-        element: (
-          <Suspense fallback={<div>Loading...</div>}>
-            <Demo1Page />
-          </Suspense>
-        ),
-      },
-    ],
-  },
-  {
-    path: '*',
-    element: <Navigate to="/" replace />,
-  },
-]);
+    basename: '/home',
+  }
+);
 
 export function App() {
   return <RouterProvider router={router} />;
